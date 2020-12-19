999a76c90ff25c2ed0de34d4bcc6e4abd6c07d9a
fridastromme <71272220+fridastromme@users.noreply.github.com>
2020-12-19 21:20:06 +0100

    Added java file

---
+++ js/frida-stroemme-pf-ma1.js
@@ -0,0 +1,36 @@
+// Question 1
+
+var food = "pizza";
+var price = 139;
+var vegetarian = true;
+
+// Question 2
+
+var firstName = "Frida";
+var lastName = "Strømme";
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+// Question 3
+
+var animal ="frog";
+var animalType = typeof(animal);
+
+console.log("The type of frog is", animalType);
+
+// Question 4
+
+var orderHasShipped = false;
+
+if (orderHasShipped === true) {
+    console.log("The order has shipped");
+} else {
+    console.log("The order did not ship");
+}
+
+// Question 5
+
+for(var i = 7; i <= 13; i++) {
+    console.log(i);
+}
---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log("Hello debugger");
+/* Console log means you're logging a message to the console for debugging, 
+whatever that means*/
+var myName;
+console.log(myName);
